abe3a5dc635b05143ca5db9a0c2a21b6b6d290a2
Mikhail Artemenko <michicosun@ya.ru>
2023-08-25 17:07:05 +0000

    add threads tests

---
+++ tests/threads/event.cpp
@@ -0,0 +1,40 @@
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include <harmony/threads/event.hpp>
+
+TEST(EventTest, Simple) {
+  harmony::threads::sync::SPSCEvent event_1;
+  harmony::threads::sync::SPSCEvent event_2;
+  std::atomic<size_t> count = 0;
+
+  auto thread_1 = std::thread([&]() {
+    for (size_t i = 0; i < 10; ++i) {
+      event_1.Wait();
+
+      ASSERT_TRUE(count.load() == 2 * i + 1);
+      count.fetch_add(1);
+
+      event_1.Reset();
+      event_2.Complete();
+    }
+  });
+
+  auto thread_2 = std::thread([&]() {
+    for (size_t i = 0; i < 10; ++i) {
+      event_2.Reset();
+
+      ASSERT_TRUE(count.load() == 2 * i);
+      count.fetch_add(1);
+
+      event_1.Complete();
+      event_2.Wait();
+    }
+  });
+
+  thread_1.join();
+  thread_2.join();
+
+  ASSERT_TRUE(count.load() == 20);
+}
---
+++ tests/threads/futex.cpp
@@ -0,0 +1,31 @@
+#include <atomic>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include <harmony/threads/futex.hpp>
+
+TEST(FutexTest, Simple) {
+  std::atomic<uint32_t> futex_cell = 0;
+  auto key = harmony::threads::futex::PrepareWake(futex_cell);
+
+  bool done = false;
+
+  auto waiter = std::thread([&]() {
+    harmony::threads::futex::Wait(futex_cell, 0);
+    done = true;
+
+    futex_cell.store(2);
+    harmony::threads::futex::WakeOne(key);
+  });
+
+  ASSERT_FALSE(done);
+
+  futex_cell.store(1);
+  harmony::threads::futex::WakeOne(key);
+  harmony::threads::futex::Wait(futex_cell, 1);
+
+  ASSERT_TRUE(done);
+
+  waiter.join();
+}
