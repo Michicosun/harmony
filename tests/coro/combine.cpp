6458db5a63fdc7af52bba4c497d56316248a48c9
Mikhail Artemenko <michicosun@ya.ru>
2023-09-03 21:13:26 +0000

    add tests for first combinator

--- tests/coro/combine.cpp
+++ tests/coro/combine.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <harmony/coro/combine/all.hpp>
+#include <harmony/coro/combine/first.hpp>
 #include <harmony/coro/core/task.hpp>
 #include <harmony/coro/run/run.hpp>
 #include <harmony/coro/run/schedule.hpp>
@@ -62,6 +63,90 @@ TEST(Coroutines, AllException) {
     };
 
     co_return co_await coro::All(task1(), task2());
+  };
+
+  ASSERT_THROW(coro::Run(amain()), std::runtime_error);
+
+  scheduler.WaitIdle();
+  scheduler.Stop();
+}
+
+TEST(Coroutines, AllExceptionImmediately) {
+  runtime::Scheduler<executors::ComputeExecutor> scheduler(12);
+  scheduler.Start();
+
+  auto amain = [&]() -> coro::Task<std::tuple<size_t, std::string>> {
+    co_await coro::Schedule(scheduler);
+
+    auto task1 = []() -> coro::Task<size_t> {
+      throw std::runtime_error("error");  // thrown in starter thread
+      co_return 10;
+    };
+
+    auto task2 = []() -> coro::Task<std::string> {
+      co_await coro::Yield();  // change thread
+      std::this_thread::sleep_for(500ms);
+      co_return "hello";
+    };
+
+    co_return co_await coro::All(task1(), task2());
+  };
+
+  ASSERT_THROW(coro::Run(amain()), std::runtime_error);
+
+  scheduler.WaitIdle();
+  scheduler.Stop();
+}
+
+TEST(Coroutines, FirstSimple) {
+  runtime::Scheduler<executors::ComputeExecutor> scheduler(12);
+  scheduler.Start();
+
+  auto amain = [&]() -> coro::Task<size_t> {
+    co_await coro::Schedule(scheduler);
+
+    auto task1 = []() -> coro::Task<size_t> {
+      co_await coro::Yield();  // change thread
+      std::this_thread::sleep_for(500ms);
+      co_return 1;
+    };
+
+    auto task2 = []() -> coro::Task<size_t> {
+      co_await coro::Yield();  // change thread
+      std::this_thread::sleep_for(200ms);
+      co_return 2;
+    };
+
+    co_return co_await coro::First(task1(), task2());
+  };
+
+  auto result = coro::Run(amain());
+
+  ASSERT_EQ(result, 2);
+
+  scheduler.WaitIdle();
+  scheduler.Stop();
+}
+
+TEST(Coroutines, FirstException) {
+  runtime::Scheduler<executors::ComputeExecutor> scheduler(12);
+  scheduler.Start();
+
+  auto amain = [&]() -> coro::Task<size_t> {
+    co_await coro::Schedule(scheduler);
+
+    auto task1 = []() -> coro::Task<size_t> {
+      co_await coro::Yield();
+      throw std::runtime_error("error");
+      co_return 1;
+    };
+
+    auto task2 = []() -> coro::Task<size_t> {
+      throw std::runtime_error("error");  // thrown in starter thread
+      co_return 2;
+    };
+
+    co_return co_await coro::First(task1(), task2());
   };
 
   ASSERT_THROW(coro::Run(amain()), std::runtime_error);
