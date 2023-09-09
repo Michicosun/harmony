65937566d2a55ceb69aa396428f8683d5aa81896
Mikhail Artemenko <michicosun@ya.ru>
2023-09-09 11:30:54 +0000

    improve example

--- examples/sync.cpp
+++ examples/sync.cpp
@@ -10,43 +10,35 @@ int main() {
   runtime::Scheduler<executors::ComputeExecutor> scheduler(12);
   scheduler.Start();
 
-  auto main = [&](size_t coro_count) -> coro::Task<int> {
+  auto amain = [&](size_t coro_count) -> coro::Task<int> {
     co_await coro::Schedule(scheduler);
 
+    coro::TaskGroup tg;
     coro::Mutex mutex;
     size_t counter = 0;
 
-    coro::WaitGroup wg;
-    wg.Add(coro_count);
-
-    auto contender = [&]() -> coro::Task<std::monostate> {
-      co_await coro::Schedule(scheduler);
+    auto runner = [&]() -> coro::Task<std::monostate> {
+      co_await coro::Yield();
 
       for (size_t j = 0; j < 100000; ++j) {
         auto lock = co_await mutex.ScopedLock();
         counter += 1;
       }
 
-      wg.Done();
-
       co_return std::monostate{};
     };
 
     for (size_t i = 0; i < coro_count; ++i) {
-      coro::Detach(contender());
+      co_await tg.Start(runner());
     }
 
-    co_await wg.Wait();
-
-    std::cout << "DONE: " << counter << std::endl;
-
+    co_await tg.Wait();
     co_return counter;
   };
 
-  coro::Run(main(1000));
+  std::cout << "DONE: " << coro::Run(amain(100)) << std::endl;
 
   scheduler.WaitIdle();
   scheduler.Stop();
-
   return 0;
 }
