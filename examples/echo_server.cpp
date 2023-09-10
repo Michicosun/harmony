339df712c40376708364f1681a9d3f50aca78d7e
Mikhail Artemenko <michicosun@ya.ru>
2023-09-10 20:57:08 +0000

    simplify example

--- examples/echo_server.cpp
+++ examples/echo_server.cpp
@@ -13,16 +13,10 @@ coro::Task<> worker(net::AcceptInfo ai) {
   std::array<std::byte, 1024> buffer;
 
   while (true) {
-    auto read_result = co_await socket.AsyncReadSome(buffer);
-    if (read_result.status == io::EventStatus::Closed) {
-      break;
-    }
-
-    size_t n = read_result.bytes_transferred;
+    size_t n = co_await socket.AsyncReadSome(buffer);
     co_await socket.AsyncWriteSome({buffer.begin(), buffer.begin() + n});
   }
 
-  std::cout << "closed " << ai.ip_address << ":" << ai.port << std::endl;
   co_return {};
 }
 
