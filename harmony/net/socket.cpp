65f4d5670df4dcf2df0e126b27045336afb565c9
Mikhail Artemenko <michicosun@ya.ru>
2023-09-15 23:22:28 +0000

    support socket connect

--- harmony/net/socket.cpp
+++ harmony/net/socket.cpp
@@ -1,7 +1,5 @@
-#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <unistd.h>
-#include <cstddef>
-#include <cstring>
 
 #include <harmony/coro/events/fd_ready.hpp>
 #include <harmony/net/exceptions.hpp>
@@ -14,11 +12,80 @@ TcpSocket::TcpSocket(io::Fd con_fd)
 }
 
 TcpSocket::~TcpSocket() {
-  close(con_fd_);
+  if (con_fd_.has_value()) {
+    close(con_fd_.value());
+  }
+}
+
+coro::Task<> TcpSocket::Connect(ConnectionParams params, size_t port) {
+  if (con_fd_.has_value()) {
+    throw NetError("socket is already connected to endpoint");
+  }
+
+  struct sockaddr_storage ss = {};
+  socklen_t addrlen;
+
+  int family = AddressFamilyToNative(params.address_family);
+  switch (family) {
+    case AF_INET: {
+      struct sockaddr_in* addr = (struct sockaddr_in*)&ss;
+      addr->sin_family = AF_INET;
+      addr->sin_port = htons(port);
+      inet_pton(AF_INET, params.ip_address.c_str(), &addr->sin_addr);
+      addrlen = sizeof(struct sockaddr_in);
+      break;
+    }
+
+    case AF_INET6: {
+      struct sockaddr_in6* addr = (struct sockaddr_in6*)&ss;
+      addr->sin6_family = AF_INET6;
+      addr->sin6_port = htons(port);
+      inet_pton(AF_INET6, params.ip_address.c_str(), &addr->sin6_addr);
+      addrlen = sizeof(struct sockaddr_in6);
+      break;
+    }
+  }
+
+  // create socket
+  con_fd_ = socket(family, SOCK_STREAM, 0);
+  if (con_fd_ < 0) {
+    throw NetError(strerror(errno));
+  }
+
+  // convert socket fd to nonblocking mode
+  io::MakeNonblocking(con_fd_.value());
+
+  // make connection
+  int code = connect(con_fd_.value(), (struct sockaddr*)&ss, addrlen);
+
+  if (code < 0 &&
+      (errno == EAGAIN || errno == EALREADY || errno == EINPROGRESS)) {
+    co_await coro::FdReady(con_fd_.value(), io::Operation::Write);
+
+    int sockopt_error;
+    socklen_t optlen = sizeof(sockopt_error);
+    if (getsockopt(con_fd_.value(), SOL_SOCKET, SO_ERROR, &sockopt_error,
+                   &optlen) < 0) {
+      throw NetError(strerror(errno));
+    }
+
+    if (sockopt_error != 0) {
+      throw NetError(strerror(sockopt_error));
+    }
+
+  } else if (code < 0) {
+    throw NetError(strerror(errno));
+  }
+
+  co_return {};
 }
 
 coro::Task<size_t> TcpSocket::AsyncReadSome(std::span<std::byte> buffer) {
-  auto status = co_await coro::FdReady(con_fd_, io::Operation::Read);
+  if (!con_fd_.has_value()) {
+    throw NetError("socket not connected to endpoint");
+  }
+
+  auto status = co_await coro::FdReady(con_fd_.value(), io::Operation::Read);
 
   if (status == io::EventStatus::Error) {
     throw NetError("error while polling descriptor");
@@ -28,7 +95,7 @@ coro::Task<size_t> TcpSocket::AsyncReadSome(std::span<std::byte> buffer) {
     throw NetError("socket was closed from the other side");
   }
 
-  int64_t n = read(con_fd_, buffer.data(), buffer.size());
+  int64_t n = read(con_fd_.value(), buffer.data(), buffer.size());
 
   if (n < 0) {
     throw NetError(strerror(errno));
@@ -38,7 +105,11 @@ coro::Task<size_t> TcpSocket::AsyncReadSome(std::span<std::byte> buffer) {
 }
 
 coro::Task<size_t> TcpSocket::AsyncWriteSome(std::span<std::byte> buffer) {
-  auto status = co_await coro::FdReady(con_fd_, io::Operation::Write);
+  if (!con_fd_.has_value()) {
+    throw NetError("socket not connected to endpoint");
+  }
+
+  auto status = co_await coro::FdReady(con_fd_.value(), io::Operation::Write);
 
   if (status == io::EventStatus::Error) {
     throw NetError("error while polling descriptor");
@@ -48,7 +119,7 @@ coro::Task<size_t> TcpSocket::AsyncWriteSome(std::span<std::byte> buffer) {
     throw NetError("socket was closed from the other side");
   }
 
-  int64_t n = write(con_fd_, buffer.data(), buffer.size());
+  int64_t n = write(con_fd_.value(), buffer.data(), buffer.size());
 
   if (n < 0) {
     throw NetError(strerror(errno));
--- harmony/net/socket.hpp
+++ harmony/net/socket.hpp
@@ -3,6 +3,7 @@
 #include <span>
 
 #include <harmony/coro/core/task.hpp>
+#include <harmony/net/net_params.hpp>
 #include <harmony/runtime/io/core/epoll_events.hpp>
 #include <harmony/runtime/io/core/fd.hpp>
 
@@ -10,15 +11,18 @@ namespace harmony::net {
 
 class TcpSocket {
  public:
+  TcpSocket() = default;
   explicit TcpSocket(io::Fd con_fd);
 
   ~TcpSocket();
+
+  coro::Task<> Connect(ConnectionParams params, size_t port);
 
   coro::Task<size_t> AsyncReadSome(std::span<std::byte> buffer);
   coro::Task<size_t> AsyncWriteSome(std::span<std::byte> buffer);
 
  private:
-  io::Fd con_fd_;
+  std::optional<io::Fd> con_fd_;
 };
 
 }  // namespace harmony::net
