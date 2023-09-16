c1e4a6e66d1de58f10d9b9541d74c33d4550532f
Mikhail Artemenko <michicosun@ya.ru>
2023-09-16 18:32:28 +0000

    move timer event source impl to cpp file

---
+++ harmony/runtime/timers/timer_event_source.cpp
@@ -0,0 +1,90 @@
+#include <harmony/runtime/timers/timer_event_source.hpp>
+
+namespace harmony::timers {
+
+void TimerEventSource::Start() {
+  worker_ = std::thread([this]() {
+    WorkerRoutine();
+  });
+}
+
+void TimerEventSource::Stop() {
+  stopped_.store(true);
+  worker_.join();
+}
+
+void TimerEventSource::AddTimer(TimerBase* timer, Deadline deadline) {
+  timer->id = next_free_id_.fetch_add(1);
+  timer->deadline = deadline;
+  new_timers_.Push(timer);
+}
+
+void TimerEventSource::AddTimer(TimerBase* timer, Duration timeout) {
+  AddTimer(timer, DeadlineFromNow(timeout));
+}
+
+void TimerEventSource::DeleteTimer(TimerBase* timer) {
+  timers_to_cancel_.Push(timer);
+}
+
+void TimerEventSource::WorkerRoutine() {
+  while (!stopped_.load()) {
+    AddNewTimers();
+    DeleteCancelledTimers();
+    ProcessTimers();
+    SuspendWorker();
+  }
+}
+
+void TimerEventSource::AddNewTimers() {
+  TimerBase* timer = new_timers_.ExtractAll();
+
+  while (timer != nullptr) {
+    timers_.push({timer, timer->id});
+    timer = Unwrap(timer->next);
+  }
+}
+
+void TimerEventSource::DeleteCancelledTimers() {
+  TimerBase* timer = timers_to_cancel_.ExtractAll();
+
+  while (timer != nullptr) {
+    deleted_timers_.insert(timer->id);
+    timer->OnFinish();
+    timer = Unwrap(timer->next);
+  }
+}
+
+void TimerEventSource::ProcessTimers() {
+  auto now = Clock::now();
+  size_t processed = 0;
+
+  while (!timers_.empty() && processed < kMaxBatchSize) {
+    const auto& timer_handle = timers_.top();
+    TimerBase* timer = timer_handle.timer;
+    uint64_t id = timer_handle.id;
+
+    if (auto it = deleted_timers_.find(id); it != deleted_timers_.end()) {
+      deleted_timers_.erase(it);  // timer is already deleted -> skipping
+      timers_.pop();
+      continue;
+    }
+
+    if (timer->deadline > now) {
+      break;
+    }
+
+    if (timer->state.Finish()) {
+      timer->OnFinish();
+      ++processed;
+    }
+
+    timers_.pop();
+  }
+}
+
+void TimerEventSource::SuspendWorker() const {
+  std::this_thread::sleep_for(kSleepTime);
+}
+
+}  // namespace harmony::timers
--- harmony/runtime/timers/timer_event_source.hpp
+++ harmony/runtime/timers/timer_event_source.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <atomic>
-#include <cstddef>
 #include <queue>
 #include <thread>
 #include <unordered_set>
@@ -9,7 +7,6 @@
 #include <harmony/runtime/timers/core/deadline.hpp>
 #include <harmony/runtime/timers/core/timer.hpp>
 #include <harmony/support/queues/batch_lock_free_queue.hpp>
-#include <harmony/threads/spin_lock/spin_lock.hpp>
 
 namespace harmony::timers {
 
@@ -24,91 +21,21 @@ class TimerEventSource {
   };
 
  public:
-  void Start() {
-    worker_ = std::thread([this]() {
-      WorkerRoutine();
-    });
-  }
+  void Start();
+  void Stop();
 
-  void Stop() {
-    stopped_.store(true);
-    worker_.join();
-  }
+  void AddTimer(TimerBase* timer, Deadline deadline);
+  void AddTimer(TimerBase* timer, Duration timeout);
 
-  void AddTimer(TimerBase* timer, Deadline deadline) {
-    timer->id = next_free_id_.fetch_add(1);
-    timer->deadline = deadline;
-    new_timers_.Push(timer);
-  }
-
-  void AddTimer(TimerBase* timer, Duration timeout) {
-    AddTimer(timer, DeadlineFromNow(timeout));
-  }
-
-  void DeleteTimer(TimerBase* timer) {
-    timers_to_cancel_.Push(timer);
-  }
+  void DeleteTimer(TimerBase* timer);
 
  private:
-  void WorkerRoutine() {
-    while (!stopped_.load()) {
-      AddNewTimers();
-      DeleteCancelledTimers();
-      ProcessTimers();
-      SuspendWorker();
-    }
-  }
+  void WorkerRoutine();
 
-  void AddNewTimers() {
-    TimerBase* timer = new_timers_.ExtractAll();
-
-    while (timer != nullptr) {
-      timers_.push({timer, timer->id});
-      timer = Unwrap(timer->next);
-    }
-  }
-
-  void DeleteCancelledTimers() {
-    TimerBase* timer = timers_to_cancel_.ExtractAll();
-
-    while (timer != nullptr) {
-      deleted_timers_.insert(timer->id);
-      timer->OnFinish();
-      timer = Unwrap(timer->next);
-    }
-  }
-
-  void ProcessTimers() {
-    auto now = Clock::now();
-    size_t processed = 0;
-
-    while (!timers_.empty() && processed < kMaxBatchSize) {
-      const auto& timer_handle = timers_.top();
-      TimerBase* timer = timer_handle.timer;
-      uint64_t id = timer_handle.id;
-
-      if (auto it = deleted_timers_.find(id); it != deleted_timers_.end()) {
-        deleted_timers_.erase(it);  // timer is already deleted -> skipping
-        timers_.pop();
-        continue;
-      }
-
-      if (timer->deadline > now) {
-        break;
-      }
-
-      if (timer->state.Finish()) {
-        timer->OnFinish();
-        ++processed;
-      }
-
-      timers_.pop();
-    }
-  }
-
-  void SuspendWorker() const {
-    std::this_thread::sleep_for(kSleepTime);
-  }
+  void AddNewTimers();
+  void DeleteCancelledTimers();
+  void ProcessTimers();
+  void SuspendWorker() const;
 
  private:
   static constexpr size_t kMaxBatchSize = 1 << 12;
